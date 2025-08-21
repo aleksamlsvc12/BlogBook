ff23f5705dc41c9467bf0840edc54bc93dda20c9
Aleksa <aleksamlsvc12@gmail.com>
2025-08-21 14:16:15 +0200

    signup page partially built

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="w-full bg-blue-600 flex justify-center items-center p-4 h-[10%]">
-    <span class="text-white text-2xl font-bold">API Integration</span>
+    <span class="text-white text-3xl font-bold">API Integration</span>
   </div>
 </template>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,3 +1,46 @@
 <template>
-  <div class="h-full bg-gray-200">a</div>
+  <div class="h-full bg-gray-200 flex justify-center items-center">
+    <div
+      class="w-[80%] sm:w-[25%] h-[80%] bg-white shadow-xl rounded-2xl flex flex-col justify-between p-10"
+    >
+      <p class="text-3xl font-bold text-center">Sing Up</p>
+
+      <form action="" class="flex flex-col h-[80%] justify-between">
+        <div class="flex flex-col gap-5">
+          <div>
+            <label for="username">Username:</label>
+            <input type="text" />
+          </div>
+
+          <div>
+            <label for="email">Email:</label>
+            <input type="text" />
+          </div>
+
+          <div>
+            <label for="password">Password:</label>
+            <input type="password" />
+            <p class="text-sm mt-2 underline cursor-pointer">Already have an account?</p>
+          </div>
+          
+          
+        </div>
+
+        <button class="bg-blue-600 text-white font-bold p-4 rounded-full cursor-pointer">
+          Sing Up
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
+
+<style>
+input {
+  border: 1px solid gray;
+  padding: 10px 15px 10px 15px;
+  border-radius: 10px;
+  display: block;
+  width: 100%;
+  margin-top: 6px;
+}
+</style>
