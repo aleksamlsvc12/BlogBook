1dd479290be67c779d001d180334a5963f022c63
Aleksa <aleksamlsvc12@gmail.com>
2025-08-21 14:50:46 +0200

    type of password input now reactive, changed the order of log in and sing up pages appearing

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,52 +1,16 @@
 <script setup>
 import { ref } from "vue";
-let show = ref(false);
+
+let showDiv = ref(false);
+let showPassword = ref(false);
 </script>
 
 <template>
   <div class="h-full bg-gray-200 flex justify-center items-center">
-    <!-- LOG IN DIV -->
-    <div
-      class="w-[80%] sm:w-[25%] h-[80%] bg-white shadow-xl rounded-2xl flex flex-col justify-between p-10"
-      v-if="show"
-    >
-      <p class="text-3xl font-bold text-center">Log In</p>
-
-      <form action="" class="flex flex-col h-[80%] justify-between">
-        <div class="flex flex-col gap-5">
-          <div>
-            <label for="username">Username:</label>
-            <input type="text" />
-          </div>
-
-          <div>
-            <label for="password">Password:</label>
-            <div class="relative">
-              <input type="password" class="bigger-padding-right" />
-              <i class="pi pi-eye absolute top-1/3 right-3 cursor-pointer"></i>
-            </div>
-
-            <p
-              class="text-sm mt-2 underline cursor-pointer"
-              @click="show = false"
-            >
-              Don't have an account?
-            </p>
-          </div>
-        </div>
-
-        <button
-          class="bg-blue-600 text-white font-bold p-4 rounded-full cursor-pointer"
-        >
-          Log In
-        </button>
-      </form>
-    </div>
-
     <!-- SIGN UP DIV -->
     <div
       class="w-[80%] sm:w-[25%] h-[80%] bg-white shadow-xl rounded-2xl flex flex-col justify-between p-10"
-      v-else
+      v-if="showDiv"
     >
       <p class="text-3xl font-bold text-center">Sing Up</p>
 
@@ -65,13 +29,20 @@ let show = ref(false);
           <div>
             <label for="password">Password:</label>
             <div class="relative">
-              <input type="password" class="bigger-padding-right" />
-              <i class="pi pi-eye absolute top-1/3 right-3 cursor-pointer"></i>
+              <input
+                :type="showPassword ? 'text' : 'password'"
+                class="bigger-padding-right"
+              />
+              <i
+                class="pi absolute top-1/3 right-3 cursor-pointer"
+                :class="showPassword ? 'pi-eye-slash' : 'pi-eye'"
+                @click="showPassword = !showPassword"
+              ></i>
             </div>
 
             <p
               class="text-sm mt-2 underline cursor-pointer"
-              @click="show = true"
+              @click="showDiv = false"
             >
               Already have an account?
             </p>
@@ -82,6 +53,51 @@ let show = ref(false);
           class="bg-blue-600 text-white font-bold p-4 rounded-full cursor-pointer"
         >
           Sing Up
+        </button>
+      </form>
+    </div>
+
+    <!-- LOG IN DIV -->
+    <div
+      class="w-[80%] sm:w-[25%] h-[80%] bg-white shadow-xl rounded-2xl flex flex-col justify-between p-10"
+      v-else
+    >
+      <p class="text-3xl font-bold text-center">Log In</p>
+
+      <form action="" class="flex flex-col h-[80%] justify-between">
+        <div class="flex flex-col gap-5">
+          <div>
+            <label for="username">Username:</label>
+            <input type="text" />
+          </div>
+
+          <div>
+            <label for="password">Password:</label>
+            <div class="relative">
+              <input
+                :type="showPassword ? 'text' : 'password'"
+                class="bigger-padding-right"
+              />
+              <i
+                class="pi absolute top-1/3 right-3 cursor-pointer"
+                :class="showPassword ? 'pi-eye-slash' : 'pi-eye'"
+                @click="showPassword = !showPassword"
+              ></i>
+            </div>
+
+            <p
+              class="text-sm mt-2 underline cursor-pointer"
+              @click="showDiv = true"
+            >
+              Don't have an account?
+            </p>
+          </div>
+        </div>
+
+        <button
+          class="bg-blue-600 text-white font-bold p-4 rounded-full cursor-pointer"
+        >
+          Log In
         </button>
       </form>
     </div>
